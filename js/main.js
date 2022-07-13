025099d5b310b2e46144b710d9ffa2525837b22f
Yevhen Khreptun <khreptun.e@gmail.com>
2022-07-13 20:00:26 +0300

    created a smooth scroll the menu items. burger menu created and header fixed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,50 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'vertical',
+// Slider testimonials
+const swiperForTestimonials = new Swiper('.swiper-testimonials', {
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
+    nextEl: '.testimonials-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  }
 });
+
+// Burger menu
+const iconMenu = document.querySelector('.header__icon');
+const menuBody = document.querySelector('.header__menu');
+
+if(iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active');
+    menuBody.classList.toggle('active');
+  });
+}
+
+// Scroll on click
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+if(menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+
+    if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const goToSection = document.querySelector(menuLink.dataset.goto);
+      const goToSectionValue = goToSection.getBoundingClientRect().top + scrollY - document.querySelector('.header').offsetHeight;
+
+      window.scrollTo({
+        top: goToSectionValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+
+    if(iconMenu.classList.contains('active')) {
+      document.body.classList.remove('lock');
+      iconMenu.classList.remove('active');
+      menuBody.classList.remove('active');
+    }
+  }  
+}
